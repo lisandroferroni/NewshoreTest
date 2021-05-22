9385147edafc94cd0795de018d6e64fbbfb76bc4
lisandroferroni <lisandro.ferroni@gmail.com>
2021-05-22 16:56:13 -0300

    Add project files.

---
+++ Data/NewshoreDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace Data
+{
+    public class NewshoreDbContext : DbContext
+    {
+        public NewshoreDbContext(DbContextOptions<NewshoreDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Transport> Transports { get; set; }
+    }
+}
---
+++ Model/Flight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Model
+{
+    public class Flight
+    {
+        public string DepartureStation { get; set; }
+        public string ArrivalStation { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public Transport Transport { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+    }
+}
